3bc378472806b1326ac27323fc05df77234fc6a6
Vargas Gonzalez <Y4072@csx.com>
2017-03-03 16:23:07 -0500

    index z

--- js/widgets.js
+++ js/widgets.js
@@ -6,11 +6,15 @@ var d = $("[id=draggable]").attr("id", function (i) {
 $.each(positions, function (id, pos) {
     $("#" + id).css(pos)
 })
-
+var highestObject = 500;
 d.draggable({obstacle: "#draggable1",
     preventCollision: true,
     containment: "#containment-wrapper",
     scroll: false,
+    start: function() {
+        highestObject++
+        $(this).css('z-index', highestObject)
+    },
     stop: function (event, ui) {
         positions[this.id] = ui.position
         localStorage.positions = JSON.stringify(positions)
