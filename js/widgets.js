b44cf586d777bb799025cd14eaeee77b33d41ba3
Vargas Gonzalez <Y4072@csx.com>
2017-03-03 14:21:50 -0500

    draggable working

---
+++ js/widgets.js
@@ -0,0 +1,18 @@
+var positions = JSON.parse(localStorage.positions || "{}");
+$(function () {
+    var d = $("[id=draggable]").attr("id", function (i) {
+        return "draggable_" + i
+    })
+    $.each(positions, function (id, pos) {
+        $("#" + id).css(pos)
+    })
+
+    d.draggable({
+        containment: "#containment-wrapper",
+        scroll: false,
+        stop: function (event, ui) {
+            positions[this.id] = ui.position
+            localStorage.positions = JSON.stringify(positions)
+        }
+    });
+});
