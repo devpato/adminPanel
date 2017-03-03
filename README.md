e556b0c19a6adfad8ef0cc5392f95e65db95a585
Vargas Gonzalez <Y4072@csx.com>
2017-03-03 16:00:16 -0500

    2 dragabble items working

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 
 http://jsfiddle.net/wearetamo/4knru705/2/
 https://jsfiddle.net/jakecigar/v685v9t6/31/
+http://jsfiddle.net/2vsvdcw4/24/
+http://www.elated.com/articles/drag-and-drop-with-jquery-your-essential-guide/
--- js/widgets.js
+++ js/widgets.js
@@ -1,18 +1,20 @@
 var positions = JSON.parse(localStorage.positions || "{}");
 $(function () {
-    var d = $("[id=draggable]").attr("id", function (i) {
-        return "draggable_" + i
-    })
-    $.each(positions, function (id, pos) {
-        $("#" + id).css(pos)
-    })
+var d = $("[id=draggable]").attr("id", function (i) {
+    return "draggable_" + i
+})
+$.each(positions, function (id, pos) {
+    $("#" + id).css(pos)
+})
 
-    d.draggable({
-        containment: "#containment-wrapper",
-        scroll: false,
-        stop: function (event, ui) {
-            positions[this.id] = ui.position
-            localStorage.positions = JSON.stringify(positions)
-        }
-    });
+d.draggable({obstacle: "#draggable1",
+    preventCollision: true,
+    containment: "#containment-wrapper",
+    scroll: false,
+    stop: function (event, ui) {
+        positions[this.id] = ui.position
+        localStorage.positions = JSON.stringify(positions)
+        //obstacle: ".obstacle", preventCollision: true
+    }
+});
 });
