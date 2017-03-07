caacbd1db758cfaf1cee1a48018d06b13cadb9c8
Vargas Gonzalez <Y4072@csx.com>
2017-03-07 15:07:11 -0500

    places machers done

--- js/widgets.js
+++ js/widgets.js
@@ -1,24 +1,28 @@
 var positions = JSON.parse(localStorage.positions || "{}");
 $(function () {
-var d = $("[id=draggable]").attr("id", function (i) {
-    return "draggable_" + i
-})
-$.each(positions, function (id, pos) {
-    $("#" + id).css(pos)
-})
-var highestObject = 500;
-d.draggable({obstacle: "#draggable1",
-    preventCollision: true,
-    containment: "#containment-wrapper",
-    scroll: false,
-    start: function() {
-        highestObject++
-        $(this).css('z-index', highestObject)
-    },
-    stop: function (event, ui) {
-        positions[this.id] = ui.position
-        localStorage.positions = JSON.stringify(positions)
-        //obstacle: ".obstacle", preventCollision: true
-    }
-});
-});
+  var d = $("[id=draggable]").attr("id", function (i) {
+      return "draggable_" + i
+  })
+  $.each(positions, function (id, pos) {
+      $("#" + id).css(pos)
+  })
+  var highestObject = 500;
+  d.draggable({obstacle: "#draggable1",
+      preventCollision: true,
+      containment: "#containment-wrapper",
+      scroll: false,
+      start: function() {
+          highestObject++
+          $(this).css('z-index', highestObject)
+      },
+      stop: function (event, ui) {
+          positions[this.id] = ui.position
+          localStorage.positions = JSON.stringify(positions)
+          //obstacle: ".obstacle", preventCollision: true
+      }
+  });
+
+
+
+
+  });
